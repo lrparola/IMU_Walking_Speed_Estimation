fe72ebeba8485b9a188c12646de7684f7eea0d33
Lauren Parola <88797808+lrparola@users.noreply.github.com>
2023-04-24 07:54:19 -0400

    Update shank_imu_speed.py

--- shank_imu_speed.py
+++ shank_imu_speed.py
@@ -18,24 +18,24 @@ data_freq = 100
 shank_length = 0.44
 thigh_length = 0.51
 leg_length = shank_length+thigh_length
+
+#calibrate all sensors to pelvis IMU
 sensor_data = pp.sensor_to_body_cal(sensor_data, data_freq)
 
-print('yes')
 
 for sensor in sensor_data:
         #calibrate imu data to gravity (x is up)
-       print(sensor)
        sensor_data[sensor] = pp.calibration_sensor_data(sensor_data,sensor, data_freq)
 shank_data = ['Left shank','Right shank']
 
-plt.plot(sensor_data['Left shank'][['Acc_X','Acc_Y','Acc_Z']])
+#filter the IMU data
 for sensor in shank_data:
     sensor_data[sensor][['Gyr_X','Gyr_Y','Gyr_Z']] = pp.butter_low(sensor_data[sensor][['Gyr_X','Gyr_Y','Gyr_Z']],data_freq, 4, 14)
     sensor_data[sensor][['Acc_X','Acc_Y','Acc_Z']] = pp.butter_low(sensor_data[sensor][['Acc_X','Acc_Y','Acc_Z']],data_freq, 4, 2)
 
 #sensor_data = pp.sensor_to_body_cal(sensor_data, data_freq)
 velocity_result = sw.find_velocity(sensor_data['Left shank']['Gyr_Y'].values, sensor_data['Left shank']['Acc_Z'].values,sensor_data['Left shank']['Acc_X'].values,data_freq) 
+
+#print average walking speed velocity
 print(str(np.mean(velocity_result))+'+/-'+str(np.std(velocity_result)))
-plt.figure()
-plt.scatter(range(len(velocity_result)),velocity_result)
-plt.show()
+
